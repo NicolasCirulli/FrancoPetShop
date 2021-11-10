a01a36a9c370adce2a3719894d792072cbc65b81
facundotecheira <facundotecheira00@gmail.com>
2021-11-10 11:37:23 -0300

    feat: comentarios

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -13,13 +13,25 @@ spanMensaje = document.getElementById('mensajeForm');
 submit.addEventListener('click', (e) => {
 
     (fullName.value && pruebaemail(email.value) == "email valido") ?
-        gato.checked || perro.checked ?
-            (divAlerta.classList = 'alert alert-success d-flex justify-content-between',
-            spanMensaje.innerText = 'Formulario enviado correctamente',
-            fullName.value = '',email.value = '',phone.value = '',mensaje.value = '',gato.checked = false,perro.checked = false) 
-            :  (divAlerta.classList = 'alert alert-success d-flex justify-content-between',
-            spanMensaje.innerText = 'Debe seleccionar al menos una mascota') :(divAlerta.classList = 'alert alert-success d-flex justify-content-between',
-            spanMensaje.innerText = 'Email no valido') ;
+        gato.checked || perro.checked ? mensaje.value ? 
+
+        (divAlerta.classList = 'alert alert-success d-flex justify-content-between',
+        spanMensaje.innerText = 'Formulario enviado correctamente',
+        fullName.value = '',email.value = '',phone.value = '',mensaje.value = '',
+        gato.checked = false,perro.checked = false)
+        
+        :(divAlerta.classList = 'alert alert-danger d-flex justify-content-between',
+        spanMensaje.innerText = 'Debe escribir un mensaje'):
+        
+        (divAlerta.classList = 'alert alert-danger d-flex justify-content-between',
+        spanMensaje.innerText = 'Debe seleccionar al menos una mascota') :
+        
+        (divAlerta.classList = 'alert alert-danger d-flex justify-content-between',
+        spanMensaje.innerText = 'Verifique que el campo nombre no esté vacio y que el email sea valido')
+            
+         
+               
+            
 
     
     e.preventDefault();
@@ -38,3 +50,17 @@ function pruebaemail(valor) {
     }
     else return ('email valido');
 }
+
+
+{/* <div class=" display-none " id="divAlerta" role="alert">
+                            <span id="mensajeForm">Formulario enviado con exitos</span>
+                            <span><a href="#" id="alerta">&#10005;</a> </span>
+                        </div>
+
+
+<div class=" form-switch mt-3 mb-3">
+<input class="form-check-input" type="checkbox" role="switch" id="gato">
+<label class="form-check-label me-5" for="flexSwitchCheckDefault">Gato</label>
+<input class="form-check-input" type="checkbox" role="switch" id="perro">
+<label class="form-check-label" for="flexSwitchCheckDefault">Perro</label>
+</div> */}
