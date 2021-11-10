27bac16a4aaa844e4dc3b77980f0a1ca47df41f0
facundotecheira <facundotecheira00@gmail.com>
2021-11-10 11:01:41 -0300

    feat: Se mejoro el codigo

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,32 +1,40 @@
 // Constantes
 const alerta = document.getElementById('alerta');
 const divAlerta = document.getElementById('divAlerta');
-const submit = document.getElementById('submit');
-const fullName = document.getElementById('clientNamelastname');
-const email = document.getElementById('clientemail');
+const submit = document.getElementById('sendMessageButton');
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone')
 const gato = document.getElementById('gato');
 const perro = document.getElementById('perro');
+const mensaje = document.getElementById('message')
+spanMensaje = document.getElementById('mensajeForm');
 
-submit.addEventListener('click',(e)=>{
+submit.addEventListener('click', (e) => {
+
+    (fullName.value && pruebaemail(email.value) == "email valido") ?
+        gato.checked || perro.checked ?
+            (divAlerta.classList = 'alert alert-success d-flex justify-content-between',
+            spanMensaje.innerText = 'Formulario enviado correctamente',
+            fullName.value = '',email.value = '',phone.value = '',mensaje.value = '',gato.checked = false,perro.checked = false) 
+            :  (divAlerta.classList = 'alert alert-success d-flex justify-content-between',
+            spanMensaje.innerText = 'Debe seleccionar al menos una mascota') :(divAlerta.classList = 'alert alert-success d-flex justify-content-between',
+            spanMensaje.innerText = 'Email no valido') ;
+
     
-    (fullName.value && pruebaemail(email.value)== "email valido")? 
-    gato.checked || perro.checked?
-    divAlerta.classList = 'alert alert-success d-flex justify-content-between' :'' :'';
-
     e.preventDefault();
 
 })
 
-alerta.addEventListener('click',(e)=>{
+alerta.addEventListener('click', (e) => {
     e.target.parentElement.parentElement.classList = 'display-none'
 })
 
 
-function pruebaemail (valor){
-    re=/^([\da-z_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/
-    if(!re.exec(valor)){
-        return('email no valido');
+function pruebaemail(valor) {
+    re = /^([\da-z_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/
+    if (!re.exec(valor)) {
+        return ('email no valido');
     }
-    else return('email valido');
-    }
-    
+    else return ('email valido');
+}
